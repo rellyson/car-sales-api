90a95aa1146fb7b99c4bb3b9beaac1cc27f907df
Rellyson Silva <rellyson@protonmail.com>
2022-05-04 19:17:33 -0300

    refact: application logger

--- application/utils/logger.go
+++ application/utils/logger.go
@@ -23,27 +23,32 @@ func NewLogger() Logger {
 	return &logger{color: color.New(color.FgWhite)}
 }
 
+// Print given message using white text color (default).
 func (l *logger) Log(m string) {
 	l.color.Println(m)
 }
 
+// Print given message using green text color.
 func (l *logger) Info(m string) {
 	l.color.Add(color.FgGreen).Println(m)
 }
 
+// Print given message using yellow text color.
 func (l *logger) Warning(m string) {
 	l.color.Add(color.FgYellow).Println(m)
 }
 
+// Print given message using red text color.
 func (l *logger) Error(m string) {
 	l.color.Add(color.FgRed).Println(m)
 }
 
-func (l *logger) Custom(m string, c color.Attribute) {
-	l.color.Add(c).Println(m)
+// Print given message using red text color followed by an os.Exit(1) call.
+func (l *logger) Fatal(m ...any) {
+	l.color.Add(color.FgHiRed).Println(m)
+	os.Exit(1)
 }
 
-func (l *logger) Fatal(m ...any) {
-	l.color.Add(color.FgRed).Println(m)
-	os.Exit(1)
+func (l *logger) Custom(m string, c color.Attribute) {
+	l.color.Add(c).Println(m)
 }
